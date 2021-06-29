e12953c118989694f1b40c35929a168f10d24f65
srkrishna3384@gmail.com <Sriramula.ramakrishna@testingxperts.com>
2021-06-29 09:23:39 +0530

    modified TestThreeDemo class

--- src/test/java/com/businesscripts/TestThreeDemo.java
+++ src/test/java/com/businesscripts/TestThreeDemo.java
@@ -3,6 +3,7 @@ package com.businesscripts;
 public class TestThreeDemo {
 	public void testThree(){
 		System.out.println("TestThreeDemo test case...");
+		System.out.println("TestThreeDemo test case...");
 	}
 
 }
