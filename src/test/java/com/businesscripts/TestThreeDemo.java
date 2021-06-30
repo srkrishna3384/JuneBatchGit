84db05797f7254d5ec0f383ed7abf737cb0438b6
Sriramula.ramakrishn <Sriramula.ramakrishna@testingxperts.com>
2021-06-30 09:12:43 +0530

    modified TestThree class

--- src/test/java/com/businesscripts/TestThreeDemo.java
+++ src/test/java/com/businesscripts/TestThreeDemo.java
@@ -5,6 +5,7 @@ public class TestThreeDemo {
 		System.out.println("TestThreeDemo test case...");
 		System.out.println("TestThreeDemo test case...");
 		System.out.println("Person2 Changes.....");
+		System.out.println("Person2 Changes.....");
 	}
 
 }
