79874e73bf6c77c041686268e356147f1af15d86
srkrishna3384@gmail.com <Sriramula.ramakrishna@testingxperts.com>
2021-06-30 09:22:41 +0530

    added testFour method

--- src/test/java/com/businesscripts/TestThreeDemo.java
+++ src/test/java/com/businesscripts/TestThreeDemo.java
@@ -6,5 +6,9 @@ public class TestThreeDemo {
 		System.out.println("TestThreeDemo test case...");
 		System.out.println("Person2 Changes.....");
 	}
+	public void testFour(){
+		System.out.println("Person1 Changes..");
+		System.out.println("TestThreeDemo test case...");
+	}
 
 }
